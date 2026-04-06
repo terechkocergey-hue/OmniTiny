5854fce0cd374841b663639562941c0031b5cded
terechkocergey-hue <terechkocergey@gmail.com>
2026-04-06 10:09:48 +0300

    Update OmniTinyMod.java

--- src/main/java/me/user/omnitiny/OmniTinyMod.java
+++ src/main/java/me/user/omnitiny/OmniTinyMod.java
@@ -8,11 +8,15 @@ import virtuoel.pehkui.api.ScaleTypes;
 public class OmniTinyMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        // Событие: когда любая сущность (включая игрока) загружается в мир
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-            if (entity.isPlayer()) {
+            // Проверяем: это игрок И его имя совпадает с твоим?
+            if (entity.isPlayer() && entity.getName().getString().equals("AkiSeen")) {
                 ScaleData scaleData = ScaleTypes.BASE.getScaleData(entity);
-                scaleData.setScale(0.25f); // 0.25 = в 4 раза меньше обычного
+                scaleData.setScale(0.25f); // Только ты будешь маленьким
+            } else if (entity.isPlayer()) {
+                // Все остальные остаются обычного размера (масштаб 1.0)
+                ScaleData scaleData = ScaleTypes.BASE.getScaleData(entity);
+                scaleData.setScale(1.0f);
             }
         });
     }
