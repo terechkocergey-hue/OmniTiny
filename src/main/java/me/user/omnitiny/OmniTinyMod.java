46ec9bac23c4ad54ffd874188897bc7a90be3c13
terechkocergey-hue <terechkocergey@gmail.com>
2026-04-06 10:03:28 +0300

    Update OmniTinyMod.java

--- src/main/java/me/user/omnitiny/OmniTinyMod.java
+++ src/main/java/me/user/omnitiny/OmniTinyMod.java
@@ -1,10 +1,19 @@
 package me.user.omnitiny;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import virtuoel.pehkui.api.ScaleData;
+import virtuoel.pehkui.api.ScaleTypes;
 
 public class OmniTinyMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        // Оставляем пустым для первой успешной сборки
+        // Когда любой игрок заходит на сервер/мир
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (entity.isPlayer()) {
+                ScaleData scaleData = ScaleTypes.BASE.getScaleData(entity);
+                scaleData.setScale(0.25f); // Уменьшаем до 25% от обычного роста
+            }
+        });
     }
 }
